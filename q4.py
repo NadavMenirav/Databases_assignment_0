7b7989c1693a8e99aa1c0b00a8d2025a25f46711
Nadav Menirav <nmenirav@gmail.com>
2025-11-07 19:29:12 +0200

    (Feat) query no. 4 returns the top 20 days with the most new cases in Israel

--- q4.py
+++ q4.py
@@ -10,6 +10,10 @@ if __name__ == '__main__':
     )
     cursor = mydb.cursor()
     cursor.execute("""
-        ## PUT YOUR QUERY HERE ##
+        SELECT date, new_cases
+        FROM covid_deaths
+        WHERE location = 'Israel'
+        ORDER BY new_cases DESC
+        LIMIT 20
     """)
     print(', '.join(str(row) for row in cursor.fetchall()))
